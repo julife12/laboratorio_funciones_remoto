d5a18ed23857235b8ca31ce6b9bedf8fb793a664
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-09-21 23:27:28 -0500

    paso 2 parte 3

--- primo.py
+++ primo.py
@@ -2,18 +2,28 @@ def is_prime(a):
     
     divisores=0
     mitadE=a//2
-    for i in range(1, mitadE):
+    for i in range(1, mitadE+1):
         if a%i==0:
             divisores=divisores+1
             if divisores==3:
                 break
 
 
-    if divisores<=1:
-        print("Is a prime number")
+    if divisores<2:
+        return 1
     else:
-        print("Is NOT a prime number") 
+        return 0 
 
 
-x=int(input("El numero a probar es: "))
-is_prime(x)
+while True:
+    try:
+        x=int(input("El numero a probar es: "))
+    except ValueError:
+        x=0
+        print(0)
+
+    if x<=0:
+        break
+    if x!=0:   
+        valor=is_prime(x)
+        print(valor)
