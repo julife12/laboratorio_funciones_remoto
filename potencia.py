9200a7db65b524d832be73011f7a3d71c0b6f342
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-09-21 22:53:20 -0500

    creo que ya casi esta

--- potencia.py
+++ potencia.py
@@ -4,10 +4,25 @@ for i in range(1, c+1):
         cuadrado=1
         for j in range(1, b+1):
             cuadrado=cuadrado*a
+
+    
         return cuadrado
-    n1=int(input("digite el numero: "))
-    if n1==0:
+
+    
+    try:
+        n1=int(input("digite el numero: "))
+        if n1==0:
+            break
+    except ValueError:
+        print("debe digitar un numero valido, intente de nuevo")
         break
-    p1=int(input("digite la potencia: "))
+
+    try:
+        p1=int(input("digite la potencia: "))
+    except ValueError:
+        print("debe digitar un numero valido, intente de nuevo")
+        break
+
     nm1=a_power_b(n1, p1)
     print("elnumero elevado es igual a: ",nm1)
+    
