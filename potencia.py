ee63e6350e0f7c65310ad49d5f58b7eff01b2bfa
julife12 <julian.jaimes.2019@upb.edu.cp>
2019-09-21 22:55:21 -0500

    listo ejercicio potencia

--- potencia.py
+++ potencia.py
@@ -1,28 +1,52 @@
-c=int(input("digite la cantidad de veces que se hara el procedimeinto: "))
-for i in range(1, c+1):
-    def a_power_b(a,b):  
-        cuadrado=1
-        for j in range(1, b+1):
-            cuadrado=cuadrado*a
+contador=1
+contador_errores=0
+contador_pares=0
+contador_impares=0
+def a_power_b(a,b):  
+    cuadrado=1
+        
+    for j in range(1, b+1):
+        cuadrado=cuadrado*a
 
     
-        return cuadrado
+    return cuadrado
 
-    
-    try:
-        n1=int(input("digite el numero: "))
-        if n1==0:
+
+while True:
+
+
+    while True:
+        
+        try:
+            n1=int(input("digite el numero: "))
             break
-    except ValueError:
-        print("debe digitar un numero valido, intente de nuevo")
-        break
+        except ValueError:
+            print("debe digitar un numero valido, intente de nuevo")
+            contador_errores=contador_errores+1
 
-    try:
-        p1=int(input("digite la potencia: "))
-    except ValueError:
-        print("debe digitar un numero valido, intente de nuevo")
+    if n1==0:
         break
+    while True:
+        try:
+            p1=int(input("digite la potencia: "))
+            break
+        except ValueError:
+            print("debe digitar un numero valido, intente de nuevo")
+            contador_errores=contador_errores+1
+    
+            
 
     nm1=a_power_b(n1, p1)
     print("elnumero elevado es igual a: ",nm1)
+    contador=contador+1
+    if nm1%2==0:
+        print("y es par")
+        contador_pares=contador_pares+1
+    else:
+        print("y es impar")
+        contador_impares=contador_impares+1
+    
+print("la cantidad de veces que se hayo la potencia fue: ", contador-1)
+print("la cantidad de errores fue: ", contador_errores)
+print("la cantidad de pares fue: ", contador_pares, " y la cantidad de impares fue: ", contador_impares)
     
